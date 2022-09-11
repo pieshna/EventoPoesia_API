5f6353d47c1647067a5ccb7f90cc0952072f7d9c
Jose Miguel Cabrera Murcia <cabreraj525@gmail.com>
2022-09-11 03:55:44 -0600

    corregimos importacion de db

--- src/connection.js
+++ src/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connection = mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(`${process.env.MONGO_URI}`)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
